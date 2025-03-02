60128dc57f1f98b35a4d4cda8339cbd891d00878
Nidhichourey <121535612+Nidhichourey@users.noreply.github.com>
2025-03-02 13:07:57 +0530

    Update Problem1.js

--- Problem1.js
+++ Problem1.js
@@ -12,24 +12,39 @@
   ]
   */
 
-  function pproductD(electronicsProducts,kitchenProducts){
-    let product_data = [...electronicsProducts, ...kitchenProducts]
-    let productdetails = product_data.map(({name,price})=>({name,price}));
-    let totalValue = product_data.reduce((total, {price , stock}) => total + (price * stock),0);
-    return {productdetails ,product_data};
+  function pproductD(product){
+      let  totalProducts = product.length;
+      let  totalValue = 0;
+      let  categorycount = 0;
+      let  totalPrice = 0;
+
+      for(let i of product){
+           totalValue += i.price * i.stock;
+           totalPrice += i.price
+
+           if(categorycount[i.category]){
+            categorycount[i.category]++;
+           }else{
+            categorycount[i.category] = 1;
+           }
+      }
+      let  averagePrice = totalPrice / totalProducts;
+      return {
+        totalValue,
+        totalProducts,
+        categorycount,
+        averagePrice
+      }
 }
 
-let electronicsProducts =[
+let product=[
     { id: 1, name: "Laptop", price: 999.99, category: "Electronics", stock: 15 },
-    { id: 2, name: "Smartphone", price: 699.99, category: "Electronics", stock: 20 }
-  ]
- 
-let kitchenProducts = [
+    { id: 2, name: "Smartphone", price: 699.99, category: "Electronics", stock: 20 },
     { id: 3, name: "Blender", price: 49.99, category: "Kitchen", stock: 10 },
     { id: 4, name: "Coffee Maker", price: 89.99, category: "Kitchen", stock: 5 }
-  ]
+]
 
-let result = pproductD(electronicsProducts,kitchenProducts)
 
-console.log(result.productdetails)
-console.log(result.totalValue)
+let result = pproductD(product)
+
+console.log(result)
